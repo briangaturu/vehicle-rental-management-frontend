75bccc60c9dae777f0a38cfad45cb218378aad51
briangaturu <briangaturu03@gmail.com>
2025-07-30 11:47:20 +0300

    online

--- src/proxxy.ts
+++ src/proxxy.ts
@@ -1,2 +1,2 @@
-// export const apiDomain = 'https://vehicle-rental-management-system.onrender.com/api/';
-export const apiDomain='http://localhost:5000/api/'
+export const apiDomain = 'https://vehicle-rental-management-system.onrender.com/api/';
+// export const apiDomain='http://localhost:5000/api/'
